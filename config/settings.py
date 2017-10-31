90dc2254de81e89378203a51e8843a42a2e923bb
boatx <>
2017-10-31 19:16:43 +0100

    Remove comments

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,9 @@
-#odswierzanie
 TARGET_FPS = 50
-#krok czasowy
 TIME_STEP = 1.0/TARGET_FPS
-#rozdzielczosc
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-#debugowanie
 DEBUG_SPRITE = 0
 
-#dzwiek
 FREQ = 44100   # same as audio CD
 BITSIZE = -16  # unsigned 16 bit
 CHANNELS = 2   # 1 == mono, 2 == stereo
