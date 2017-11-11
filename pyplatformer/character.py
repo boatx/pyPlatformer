fac2c83c6eaead6b557d46413255ed0828220d43
boatx <>
2017-11-11 18:28:47 +0100

    Control character using states

--- pyplatformer/character.py
+++ pyplatformer/character.py
@@ -1,41 +1,97 @@
+from collections import defaultdict
+from enum import Enum
+
 from pygame import sprite, transform
 
 from pyplatformer.config.const import GRAVITY
 from pyplatformer.helpfun import load_image
 
 
+class Orientation(Enum):
+    LEFT = -1
+    RIGHT = 1
+
+
+class State(Enum):
+    NORMAL = 1
+    JUMP = 2
+    DOUBLE_JUMP = 3
+    WALK = 4
+
+
+def change_img_orientation(img, orientation=Orientation.LEFT):
+    return transform.flip(img, orientation.value, 0)
+
+
 class Character(sprite.Sprite):
     """Character class"""
 
+    COUNTER_DIVIDER = 10
+    DEFAULT_SPEED = 1
+
     def __init__(self, area):
-        sprite.Sprite.__init__(self)
+        super().__init__()
 
-        self.image_normal1_r, self.rect_sprite = load_image("normal.png")
-        self.image_normal1_l = transform.flip(self.image_normal1_r, 1, 0)
-        self.image_walk1_r, _ = load_image("walk1.png")
-        self.image_walk1_l = transform.flip(self.image_walk1_r, 1, 0)
-        self.image_walk2_r, _ = load_image("walk2.png")
-        self.image_walk2_l = transform.flip(self.image_walk2_r, 1, 0)
-        self.image_jump_r, _ = load_image("jump.png")
-        self.image_jump_l = transform.flip(self.image_jump_r, 1, 0)
-
-        self.image, self.rect = self.image_normal1_r, self.rect_sprite
-        self.rect.midbottom = area.midbottom
         self.area = area
-        self.status = "normal"
+        self.state = State.NORMAL
+        self.orientation = Orientation.RIGHT
+        self.images, self.rect_sprite = self.load_images()
+        self.image = self.images[self.state][self.orientation]
+        self.rect = self.rect_sprite
+        self.rect.midbottom = area.midbottom
         self.x = self.rect.x
         self.y = self.rect.y
         self.vel_x = 0.0
         self.vel_y = 0.0
-        self.orientation = "right"
-        self.speed = 1
+        self.speed = self.DEFAULT_SPEED
         self.speed_max = 5
         self.speed_increase = 0.2
         self.jump_speed = -20
         self.counter = 0
 
+    def load_images(self):
+        images = defaultdict(dict)
+        img, rect = load_image("normal.png")
+        images[State.NORMAL][Orientation.RIGHT] = img
+        images[State.NORMAL][Orientation.LEFT] = change_img_orientation(img)
+
+        img, _ = load_image("jump.png")
+        images[State.JUMP][Orientation.RIGHT] = img
+        images[State.JUMP][Orientation.LEFT] = change_img_orientation(img)
+        images[State.DOUBLE_JUMP] = images[State.JUMP]
+
+        img1, _ = load_image("walk1.png")
+        img2, _ = load_image("walk2.png")
+        images[State.WALK][Orientation.RIGHT] = [img1, img2]
+        images[State.WALK][Orientation.LEFT] = [
+            change_img_orientation(img1), change_img_orientation(img2)]
+
+        return images, rect
+
+    @property
+    def is_in_air(self):
+        return self.state in (State.JUMP, State.DOUBLE_JUMP)
+
+    def update_image(self, frame=None):
+        image = self.images[self.state][self.orientation]
+
+        if isinstance(image, list):
+            image = self.select_frame(image, frame=frame)
+
+        self.image = image
+
+    def select_frame(self, image, frame=None):
+        if frame is not None:
+            self.counter = 0
+            return image[frame]
+        if self.counter == self.COUNTER_DIVIDER*len(image):
+            self.counter = 0
+        index = self.counter // self.COUNTER_DIVIDER
+        self.counter += 1
+        return image[index]
+
     def update(self):
-        if self.status == "jump" or self.status == "double_jump":
+        if self.is_in_air:
             # gravity
             self.vel_y += GRAVITY
 
@@ -44,58 +100,39 @@ class Character(sprite.Sprite):
             if self.vel_x != 0.0 and self.speed < self.speed_max:
                 self.speed += self.speed_increase
 
-        if self.vel_x != 0.0 and self.vel_y == 0.0:
-            if self.counter > 10:
-                self.counter = 0
-                if self.orientation == "left":
-                    if self.image == self.image_walk1_l:
-                        self.image = self.image_walk2_l
-                    else:
-                        self.image = self.image_walk1_l
-
-                if self.orientation == "right":
-                    if self.image == self.image_walk1_r:
-                        self.image = self.image_walk2_r
-                    else:
-                        self.image = self.image_walk1_r
-            else:
-                self.counter += 1
-
         # stop at the bottom
         if self.rect.bottom > self.area.bottom:
             self.rect.bottom = self.area.bottom
             self.vel_y = 0
-            self.status = "normal"
-            if self.orientation == "right":
-                self.image = self.image_normal1_r
-            else:
-                self.image = self.image_normal1_l
+            self.state = State.NORMAL if self.vel_x == 0 else State.WALK
+
+        self.update_image()
 
     def move(self, vel_x):
         self.vel_x = vel_x
         self.turn()
+        self.counter = 0
+        if not self.is_in_air:
+            self.state = State.WALK
 
     def turn(self):
-        if self.vel_x < 0 and self.orientation == "right":
-            self.orientation = "left"
-            self.image = self.image_walk1_l
-        elif self.vel_x > 0 and self.orientation == "left":
-            self.orientation = "right"
-            self.image = self.image_walk1_r
+        if self.vel_x < 0 and self.orientation == Orientation.RIGHT:
+            self.orientation = Orientation.LEFT
+        elif self.vel_x > 0 and self.orientation == Orientation.LEFT:
+            self.orientation = Orientation.RIGHT
 
     def jump(self):
-        if self.status != "double_jump":
-            if self.status == "jump":
-                self.status = "double_jump"
-                self.vel_y = self.jump_speed
-            else:
-                self.status = "jump"
-                if self.orientation == "right":
-                    self.image = self.image_jump_r
-                else:
-                    self.image = self.image_jump_l
-                self.vel_y = self.jump_speed
+        if self.state == State.DOUBLE_JUMP:
+            return
+        if self.state == State.JUMP:
+            self.state = State.DOUBLE_JUMP
+            self.vel_y = self.jump_speed
+        else:
+            self.state = State.JUMP
+            self.vel_y = self.jump_speed
 
     def stop(self):
         self.vel_x = 0.0
-        self.speed = 1
+        self.speed = self.DEFAULT_SPEED
+        if not self.is_in_air:
+            self.state = State.NORMAL
