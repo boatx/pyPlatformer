1070e0552e9e8c4e0c08adedb651e4f67669440f
boatx <>
2017-12-17 00:14:50 +0100

    Update requirements, add single script to run client/server

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
-from pyplatformer.game import Game
+import logging
 
-if __name__ == "__main__":
+from aiohttp import web
+from command_manager import Manager
+
+from pyplatformer.game import Game
+from pyplatformer.game_client import GameClient
+from pyplatformer.game_server import GameServer
+
+
+logging.basicConfig(level=logging.INFO)
+
+manager = Manager()
+
+
+@manager.command
+def single():
     game = Game()
     game.initialize_screen()
     game.run()
+
+
+@manager.option('--port', default=8888, type=int)
+@manager.option('--host', default='localhost', type=str)
+def client(host, port):
+    client = GameClient(server_addres='http://{}:{}/'.format(host, port))
+    client.run()
+
+
+@manager.option('--port', default=8888, type=int)
+@manager.option('--host', default='localhost', type=str)
+def server(host, port):
+    game_server = GameServer()
+    app = game_server.get_app()
+    web.run_app(app, host=host, port=port)
+
+
+if __name__ == "__main__":
+    manager.run_command()
